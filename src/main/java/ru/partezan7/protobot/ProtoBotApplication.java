c0d8c6cfb1a13465addd0c85f17a81f68f98053b
partezan7 <partezan7@rambler.ru>
2023-03-06 01:40:45 +0300

    Add telegram bot

---
+++ src/main/java/ru/partezan7/protobot/ProtoBotApplication.java
@@ -0,0 +1,22 @@
+package ru.partezan7.protobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+public class ProtoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtoBotApplication.class, args);
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
